d527dc429f04b73748b0ff17c0ef1f7eb20268fe
amitdahan64 <amit.dahan01@post.runi.ac.il>
2024-02-07 18:00:56 +0200

    Update PlayList.java

--- PlayList.java
+++ PlayList.java
@@ -121,7 +121,7 @@ class PlayList {
         //// replace this comment with your code
         if(i < 0 || size == maxSize-1 || i > maxSize){return;}
         tracks[i] = null;
-        for(int j = i; j <= size-1; j++){
+        for(int j = i; j < size; j++){
             tracks[j] = tracks[j+1];
         }
         tracks[size-1] = null;
@@ -174,9 +174,9 @@ class PlayList {
             for(int i = 0; i < other.getSize(); i++)
             {
                 tracks[size + i] = other.getTrack(i);
-                size++;
+                
             }
-            
+            size += other.getSize();
             
         }
     }
